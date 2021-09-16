97d27e35a4d8098e843f775c86fcf1e31dceebf7
triyono <yono.tri@gmail.com>
2021-09-16 21:29:28 +0700

    add crud

--- orders.go
+++ orders.go
@@ -6,6 +6,7 @@ import (
 	"github.com/swaggo/http-swagger"
 	"log"
 	"net/http"
+	"strconv"
 	_ "swager-master/docs"
 	"time"
 )
@@ -23,7 +24,7 @@ type Item struct {
 	Quantity    string `json:"quantity" example:"1" `
 }
 var orders []Order
-//var prevOrderID = 0
+var prevOrderID = 0
 
 // @title Orders API
 // @version 1.0
@@ -39,8 +40,8 @@ var orders []Order
 func main() {
 	router := mux.NewRouter()
 
-	//router.HandleFunc("/orders",createOrder).Methods("POST")
-	//router.HandleFunc("/orders/{orderId}",getOrder).Methods("GET")
+	router.HandleFunc("/orders",createOrder).Methods("POST")
+	router.HandleFunc("/orders/{orderId}",getOrder).Methods("GET")
 	router.HandleFunc("/orders", getOrders).Methods("GET")
 	//router.HandleFunc("/orders/{orderId}",updateOrder).Methods("PUT")
 	//router.HandleFunc("/orders/{orderId}",deleteOrder).Methods("DELETE")
@@ -59,4 +60,93 @@ func main() {
 func getOrders(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(orders)
+}
+
+// CreateOrder godoc
+// @Summary Create a new order
+// @Description Create a new order with the input paylod
+// @Tags orders
+// @Accept  json
+// @Produce  json
+// @Param order body Order true "Create order"
+// @Success 200 {object} Order
+// @Router /orders [post]
+func createOrder(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var order Order
+	json.NewDecoder(r.Body).Decode(&order)
+	prevOrderID++
+	order.OrderID = strconv.Itoa(prevOrderID)
+	orders = append(orders, order)
+	json.NewEncoder(w).Encode(order)
+}
+
+// GetOrder godoc
+// @Summary Get details for a given orderId
+// @Description Get details of order corresponding to the input orderId
+// @Tags orders
+// @Accept  json
+// @Produce  json
+// @Param orderId path int true "ID of the order"
+// @Success 200 {object} Order
+// @Router /orders/{orderId} [get]
+func getOrder(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	inputOrderID := params["orderId"]
+	for _, order := range orders {
+		if order.OrderID == inputOrderID {
+			json.NewEncoder(w).Encode(order)
+			return
+		}
+	}
+}
+
+
+// UpdateOrder godoc
+// @Summary Update order identified by the given orderId
+// @Description Update the order corresponding to the input orderId
+// @Tags orders
+// @Accept  json
+// @Produce  json
+// @Param orderId path int true "ID of the order to be updated"
+// @Success 200 {object} Order
+// @Router /orders/{orderId} [put]
+func updateOrder(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	inputOrderID := params["orderId"]
+	for i, order := range orders {
+		if order.OrderID == inputOrderID {
+			orders = append(orders[:i], orders[i+1:]...)
+			var updatedOrder Order
+			json.NewDecoder(r.Body).Decode(&updatedOrder)
+			orders = append(orders, updatedOrder)
+			json.NewEncoder(w).Encode(updatedOrder)
+			return
+		}
+	}
+}
+
+
+// DeleteOrder godoc
+// @Summary Delete order identified by the given orderId
+// @Description Delete the order corresponding to the input orderId
+// @Tags orders
+// @Accept  json
+// @Produce  json
+// @Param orderId path int true "ID of the order to be deleted"
+// @Success 204 "No Content"
+// @Router /orders/{orderId} [delete]
+func deleteOrder(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	inputOrderID := params["orderId"]
+	for i, order := range orders {
+		if order.OrderID == inputOrderID {
+			orders = append(orders[:i], orders[i+1:]...)
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+	}
 }
